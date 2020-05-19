1e8e66867b9063e9da76d685717d8fba492f81b5
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 12:50:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,9 @@ class Transfer
   end 
   
   def reverse_transfer 
-    
+    if self.status == "complete"
+      sender.balance += self.amount 
+      receiver.balance -= self.amount
+    end 
   end 
 end
