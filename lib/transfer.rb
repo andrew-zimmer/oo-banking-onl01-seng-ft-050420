73fdf2162fa6fcd22315caaa6a1c21757ff02924
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 12:35:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,14 +8,6 @@ class Transfer
   end 
  
   def valid? 
-    if (self.sender.balance - amount) > 0 && self.sender.status == "open" 
-      if self.receiver.balance > 0 && self.receiver.status == "open"
-        true 
-      else 
-        false 
-      end 
-    else 
-      false 
-    end 
+    sender.valid? && receiver.valid? ? true : falseS
   end 
 end
